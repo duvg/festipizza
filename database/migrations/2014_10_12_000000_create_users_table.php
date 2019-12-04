a3d46d09f9df1c1bb63c52cc3bef636760d39f57
duviel <duviel7@gmail.com>
2019-12-04 16:05:16 -0500

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rols', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name', 25);
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+
+            $table->unsignedInteger('rol_id');
+
+            $table->foreign('rol_id')->references('id')->on('rols');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
